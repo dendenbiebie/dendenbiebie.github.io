f9527d8b4a6181903f4f20a8dd97f8c3a80fb41b
dendenbiebie <dendenbiebie@gmail.com>
2023-03-02 16:51:52 +0700

    frontend alpha

---
+++ precache-manifest.82150942bc2457451f4c84663591663c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "260e8803ad4303aff90dacee0cf40310",
+    "url": "/index.html"
+  },
+  {
+    "revision": "28f5da92f7e8e3fb266e",
+    "url": "/static/css/2.3d433e25.chunk.css"
+  },
+  {
+    "revision": "95e5e79fb456a7ef26ae",
+    "url": "/static/css/main.70568ac4.chunk.css"
+  },
+  {
+    "revision": "28f5da92f7e8e3fb266e",
+    "url": "/static/js/2.0cfdf5c9.chunk.js"
+  },
+  {
+    "revision": "4eb87ac7238d3655e6d95ac8ff3be3ce",
+    "url": "/static/js/2.0cfdf5c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95e5e79fb456a7ef26ae",
+    "url": "/static/js/main.913ff284.chunk.js"
+  },
+  {
+    "revision": "3f8b7f4c04e766f3b98e",
+    "url": "/static/js/runtime-main.3486abd2.js"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/FontAwesome.b06871f2.ttf"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/static/media/MaterialIcons.a37b0c01.ttf"
+  }
+]);
